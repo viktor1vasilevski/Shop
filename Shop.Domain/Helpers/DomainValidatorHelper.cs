dc707d1315624220559274eb7726d3dfb1aeeec8
Viktor <vasilevskiviktor@yahoo.com>
2025-12-05 00:39:37 +0100

    add domain validation helper

---
+++ Shop.Domain/Helpers/DomainValidatorHelper.cs
@@ -0,0 +1,18 @@
+﻿using Shop.Domain.Exceptions;
+
+namespace Shop.Domain.Helpers;
+
+public static class DomainValidatorHelper
+{
+    public static void ThrowIfNullOrWhiteSpace(string value, string propertyName)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new DomainValidationException($"{propertyName} cannot be empty.");
+    }
+
+    public static void ThrowIfTooLong(string value, int maxLength, string propertyName)
+    {
+        if (value.Length > maxLength)
+            throw new DomainValidationException($"{propertyName} cannot exceed {maxLength} characters.");
+    }
+}
--- Shop.Domain/Models/Product.cs
+++ Shop.Domain/Models/Product.cs
@@ -1,11 +1,36 @@
-﻿using Shop.Domain.Models.Base;
+﻿using Shop.Domain.Exceptions;
+using Shop.Domain.Helpers;
+using Shop.Domain.Models.Base;
 
 namespace Shop.Domain.Models;
 
 public class Product : AuditableBaseEntity
 {
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public int Quantity { get; set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public int Quantity { get; private set; }
 
+
+    private Product() { }
+    public static Product Create(string name, string description, int quantity)
+    {
+        DomainValidatorHelper.ThrowIfNullOrWhiteSpace(name, nameof(name));
+
+        DomainValidatorHelper.ThrowIfTooLong(name, 200, nameof(name));
+
+        DomainValidatorHelper.ThrowIfNullOrWhiteSpace(description, nameof(description));
+
+        DomainValidatorHelper.ThrowIfTooLong(description, 1000, nameof(description));
+
+        if (quantity < 0)
+            throw new DomainValidationException("Product quantity cannot be negative.");
+
+        var product = new Product();
+
+        product.Name = name.Trim();
+        product.Description = description.Trim();
+        product.Quantity = quantity;
+
+        return product;
+    }
 }
