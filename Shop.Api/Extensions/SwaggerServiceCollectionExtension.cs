415a2fa412ba5a2ae4d68074f4603e112928fab8
Viktor <vasilevskiviktor@yahoo.com>
2025-12-04 20:59:19 +0100

    add swagger

---
+++ Shop.Api/Extensions/SwaggerServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.OpenApi;
+
+namespace Shop.Api.Extensions;
+
+public static class SwaggerServiceCollectionExtension
+{
+    public static IServiceCollection AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Shop.Api",
+                Version = "v1"
+            });
+        });
+
+        return services;
+    }
+}
--- Shop.Api/Program.cs
+++ Shop.Api/Program.cs
@@ -1,3 +1,5 @@
+using Shop.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,16 +8,29 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwagger();
+
+builder.Services.AddCors(policy => policy.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop.Api");
+    });
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("MyPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
