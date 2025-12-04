520c6c501299ea564a9b5432f58cc3ec45ee03e6
Viktor <vasilevskiviktor@yahoo.com>
2025-12-05 00:29:09 +0100

    small middle fix

---
+++ Shop.Api/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Shop.Application.Enums;
+using Shop.Application.Responses.Base;
+
+namespace Shop.Api.Middlewares
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception occurred: {Message}", ex.Message);
+
+                var response = new ApiResponse<object>
+                {
+                    Data = null,
+                    Message = "An unexpected error occurred.",
+                    Status = ResponseStatus.Error,
+                };
+
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(response);
+            }
+        }
+    }
+
+
+}
