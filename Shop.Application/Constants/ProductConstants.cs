11460eed7f627cf592905ecfafa89ba1ae8153b5
Viktor <vasilevskiviktor@yahoo.com>
2025-12-05 00:40:10 +0100

    add product constants

---
+++ Shop.Application/Constants/ProductConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Application.Constants;
+
+public static class ProductConstants
+{
+    public const string ProductSuccessfullyCreated = "Product was successfully created.";
+    public const string ProductSuccessfullyUpdated = "Product was successfully updated.";
+    public const string ProductSuccessfullyDeleted = "Product was successfully deleted.";
+
+    public const string ProductExist = "Product with that name already exists.";
+}
