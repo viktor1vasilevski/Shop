ce7d783fd004912013c36f8029cf56e366ad969e
Viktor <vasilevskiviktor@yahoo.com>
2025-12-05 00:40:26 +0100

    add create product endpoint

--- Shop.Api/Controllers/ProductController.cs
+++ Shop.Api/Controllers/ProductController.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Interfaces;
+using Shop.Application.Requests;
+using Shop.Application.Responses.Base;
+using Shop.Application.Responses.Product;
 
 namespace Shop.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ProductController : BaseController
+public class ProductController(IProductService _productService) : BaseController
 {
+
+
+    [HttpPost]
+    public async Task<ActionResult<ApiResponse<ProductResponseDto>>> Create([FromBody] CreateProductRequest request, CancellationToken cancellationToken)
+    {
+        var response = await _productService.CreateProductAsync(request, cancellationToken);
+        return HandleResponse(response);
+    }
 }
