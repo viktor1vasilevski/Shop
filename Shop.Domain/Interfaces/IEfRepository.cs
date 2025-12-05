97a967003342566bde15c043326b437699c8eb8b
Viktor <vasilevskiviktor@yahoo.com>
2025-12-05 13:03:07 +0100

    add asNoTracking feature

--- Shop.Domain/Interfaces/IEfRepository.cs
+++ Shop.Domain/Interfaces/IEfRepository.cs
@@ -4,12 +4,11 @@ namespace Shop.Domain.Interfaces;
 
 public interface IEfRepository<TEntity> where TEntity : class
 {
-
-    Task<TEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
-    Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>>? predicate = null, CancellationToken cancellationToken = default);
+    Task<TEntity?> GetByIdAsync(Guid id, bool asNoTracking = true, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> GetAllAsync(bool asNoTracking = true, CancellationToken cancellationToken = default);
+    Task<IEnumerable<TEntity>> FindAsync(Expression<Func<TEntity, bool>>? predicate = null, bool asNoTracking = true, CancellationToken cancellationToken = default);
     Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
     void Update(TEntity entity);
     void Delete(TEntity entity);
-    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>>? predicate = null, CancellationToken cancellationToken = default);   
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>>? predicate = null, bool asNoTracking = true, CancellationToken cancellationToken = default);
 }
