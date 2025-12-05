c858df1cae710e3d62ddda5f899a8bc4d3753323
Viktor <vasilevskiviktor@yahoo.com>
2025-12-05 19:45:21 +0100

    add DbUpdateException in the global exception handler

--- Shop.Api/Middlewares/GlobalExceptionHandlerMiddleware.cs
+++ Shop.Api/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using Shop.Application.Enums;
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Application.Constants;
+using Shop.Application.Enums;
 using Shop.Application.Responses.Base;
 
 namespace Shop.Api.Middlewares;
@@ -10,6 +12,32 @@ public class GlobalExceptionHandlerMiddleware(RequestDelegate _next, ILogger<Glo
         try
         {
             await _next(context);
+        }
+        catch(DbUpdateException ex)
+        {
+            var message = ex.InnerException?.Message ?? ex.Message;
+            if (message.Contains("IX_Products_Name", StringComparison.OrdinalIgnoreCase))
+            {
+                context.Response.StatusCode = StatusCodes.Status409Conflict;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(new ApiResponse<object>
+                {
+                    Data = null,
+                    Message = ProductConstants.ProductExist,
+                    Status = ResponseStatus.Conflict
+                });
+            }
+            if (message.Contains("CK_Product_Quantity_NonNegative", StringComparison.OrdinalIgnoreCase))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(new ApiResponse<object>
+                {
+                    Data = null,
+                    Message = "Quantity cannot be negative.",
+                    Status = ResponseStatus.BadRequest
+                });
+            }
         }
         catch (Exception ex)
         {
