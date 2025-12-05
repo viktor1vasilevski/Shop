98830d264a60b2e0a244ff913577610b815b36e5
Viktor <vasilevskiviktor@yahoo.com>
2025-12-05 13:04:08 +0100

    add update request

---
+++ Shop.Application/Requests/UpdateProductRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Shop.Application.Requests;
+
+public class UpdateProductRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+}
