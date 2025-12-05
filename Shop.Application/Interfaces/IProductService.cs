b4e0e54c5a9b2056f56b18d20fa9b5b12666cd18
Viktor <vasilevskiviktor@yahoo.com>
2025-12-05 13:04:41 +0100

    add crud method in product service interface

--- Shop.Application/Interfaces/IProductService.cs
+++ Shop.Application/Interfaces/IProductService.cs
@@ -7,4 +7,8 @@ namespace Shop.Application.Interfaces;
 public interface IProductService
 {
     Task<ApiResponse<ProductResponseDto>> CreateProductAsync(CreateProductRequest request, CancellationToken cancellationToken = default);
+    Task<ApiResponse<List<ProductResponseDto>>> GetProductsAsync(CancellationToken cancellationToken = default);
+    Task<ApiResponse<ProductResponseDto>> GetProductByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<ApiResponse<ProductResponseDto>> UpdateProductAsync(Guid id, UpdateProductRequest request, CancellationToken cancellationToken = default);
+    Task<ApiResponse<object>> DeleteProductAsync(Guid id, CancellationToken cancellationToken = default);
 }
